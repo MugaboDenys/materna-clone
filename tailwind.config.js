091e8ccbf46a485324c38828eb216ced0272cf3c
MugaboDenys <mugabodan2020@gmail.com>
2022-10-07 15:49:45 +0200

    sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'roboto' : ['Roboto Condensed', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
